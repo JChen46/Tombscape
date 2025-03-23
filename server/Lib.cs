dc8bf9050574d22eeb522d8112c6f8b1c7ab387a
silso <chroelo@gmail.com>
2025-03-23 17:28:48 -0500

    Added movement actions

--- server/Lib.cs
+++ server/Lib.cs
@@ -1,4 +1,5 @@
 using SpacetimeDB;
+using Index = SpacetimeDB.Index;
 
 public static partial class Module
 {
@@ -20,9 +21,9 @@ public static partial class Module
     [Table(Name = "npc", Public = true)]
     public partial struct Npc
     {
-        [PrimaryKey, AutoInc] public uint entity_id;
-        [SpacetimeDB.Index.BTree] public uint npc_id;
-        [SpacetimeDB.Index.BTree] public uint player_id;
+        [PrimaryKey] public uint entity_id;
+        [Index.BTree] public uint npc_id;
+        [Index.BTree] public uint player_id;
         
     }
     
@@ -36,6 +37,38 @@ public static partial class Module
         public uint player_id;
         public string name;
     }
+
+    [Table(Name = "player_character", Public = true)]
+    public partial struct PlayerCharacter
+    {
+        [PrimaryKey] public uint entity_id;
+        [Unique] public uint player_id;
+    }
+
+    [Table(Name = "action", Public = true)]
+    public partial struct Action
+    {
+        [PrimaryKey, AutoInc] public uint action_id;
+        [Index.BTree] public ulong tick_id;
+        public string type;
+    }
+
+    [Table(Name = "movement_action", Public = true)]
+    public partial struct MovementAction
+    {
+        [PrimaryKey] public uint action_id;
+        [Unique] public uint player_id;
+        public DbVector2 position;
+    }
+
+    [Table(Name = "tick_timer", Scheduled = nameof(EndTick), ScheduledAt = nameof(scheduled_at))]
+    public partial struct TickTimer
+    {
+        [PrimaryKey, AutoInc] public ulong scheduled_id;
+        [Unique, AutoInc] public ulong tick_id; // Need to be different from scheduled_id?
+        public ScheduleAt scheduled_at;
+    }
+    
     
     
     // ================================ Reducers ================================
@@ -45,6 +78,10 @@ public static partial class Module
     {
         Log.Info($"Initializing...");
         ctx.Db.config.Insert(new Config { world_size = DEFAULT_WORLD_SIZE });
+        ctx.Db.tick_timer.Insert(new TickTimer
+        {
+            scheduled_at = new ScheduleAt.Interval(TimeSpan.FromMilliseconds(600))
+        });
     }
 
     [Reducer]
@@ -62,5 +99,39 @@ public static partial class Module
         player.name = name;
         Log.Info($"TestUpdate :: player {ctx.Sender} updated to {name}");
         ctx.Db.player.identity.Update(player);
+    }
+
+    [Reducer]
+    public static void EndTick(ReducerContext ctx, TickTimer tick)
+    {
+        Log.Info($"EndTick...");
+        foreach (var action in ctx.Db.action.tick_id.Filter(tick.tick_id))
+        {
+            switch (action.type)
+            {
+                case "movement":
+                    MoveAction(ctx, action);
+                    break;
+            }
+        }
+    }
+
+    [Reducer]
+    public static void MoveAction(ReducerContext ctx, Action action)
+    {
+        var movementAction = ctx.Db.movement_action.action_id.Find(action.action_id);
+        if (movementAction.HasValue)
+        {
+            var player = ctx.Db.player.player_id.Find(movementAction.Value.player_id) ?? throw new Exception("Player not found");
+            var pc = ctx.Db.player_character.player_id.Find(player.player_id) ?? throw new Exception("Player not found");
+            var entity = ctx.Db.entity.entity_id.Find(pc.entity_id) ?? throw new Exception("Entity not found");
+            var distance = movementAction.Value.position - entity.position;
+            if (Math.Abs(distance.x) <= 2 && Math.Abs(distance.y) <= 2)
+            {
+                Log.Info($"Moving to {movementAction.Value.position}");
+                entity.position = movementAction.Value.position;
+                ctx.Db.entity.entity_id.Update(entity);
+            }
+        }
     }
 }
